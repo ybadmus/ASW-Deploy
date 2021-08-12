5a597b951cd7658c7371d95cc39183b878b6a2f6
ybadmus <yusif.badmus@gmail.com>
2021-08-12 15:46:23 +0000

    Updates

---
+++ precache-manifest.ec0de0ddbf4cd4d2c6b0c424e70a677c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462eb199fe1fe107aa93d2023452487c",
+    "url": "/asw-gh-pages/index.html"
+  },
+  {
+    "revision": "20004464c51ca14c3ed2",
+    "url": "/asw-gh-pages/static/css/2.841443a6.chunk.css"
+  },
+  {
+    "revision": "f80b7f1924f184f71e0f",
+    "url": "/asw-gh-pages/static/css/main.3d0ad4d0.chunk.css"
+  },
+  {
+    "revision": "20004464c51ca14c3ed2",
+    "url": "/asw-gh-pages/static/js/2.b121e1e4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/asw-gh-pages/static/js/2.b121e1e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80b7f1924f184f71e0f",
+    "url": "/asw-gh-pages/static/js/main.679d5168.chunk.js"
+  },
+  {
+    "revision": "d7288d11f08522a1ac68",
+    "url": "/asw-gh-pages/static/js/runtime-main.f3d9b48f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/asw-gh-pages/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/asw-gh-pages/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/asw-gh-pages/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/asw-gh-pages/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/asw-gh-pages/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
